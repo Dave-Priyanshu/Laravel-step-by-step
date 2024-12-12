4d7a0d4c81089123a712d6f202170310b955d1ea
Dave-Priyanshu <davepriyanshu2001@gmail.com>
2024-12-12 16:45:28 +0530

    Added Dedicated Controller Classes

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index(){
+        $jobs = Job::with('employer')->latest()->paginate(3);
+
+        return view('jobs.index',[
+            'jobs'=> $jobs
+        ]);
+    }
+
+    public function create(){
+        return view('jobs.create');
+    }
+
+    public function show(Job $job){
+        return view('jobs.show', ['job' => $job]);
+    }
+  
+    public function store(Request $request){
+        // validation
+    $request->validate([
+        'title'=> 'required|string|min:3',
+        'salary'=> 'required|numeric'
+    ]);
+
+    Job::create([
+        'title'=> request('title'),
+        'salary'=> request('salary'),
+        'employer_id'=> 1
+    ]);
+
+    return redirect('/jobs');
+    }
+
+    public function edit(Job $job){
+        /*   $job = Job::find($job);  
+        We do not need to find the job using the id anymore,because we are using route model binding*/
+
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    public function update(Request $request, Job $job){
+        // validation
+        $request->validate([
+            'title'=> 'required|min:3',
+            'salary'=> 'required'
+        ]);
+
+        // authorization(on hold)..
+        // update the job
+        //! method 1
+        /*    $job = Job::findorFail($id); 
+        We do not need to find the job using the id anymore,because we are using route model binding*/
+
+        // $job->title = $request('title');
+        // $job->salary= $request('salary');
+        // $job->save();
+
+        //! method 2
+        $job->update([
+            'title'=> request('title'),
+            'salary'=> request('salary')
+        ]);
+        // redirect to job page
+        return redirect('/jobs/'.$job->id);
+
+    }
+
+    public function destroy(job $job){
+        // autorize the req(on hold)
+
+        // $job = Job::findorFail($id);
+        $job->delete();
+
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Models\Employer;
-use Illuminate\Support\Arr;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
-use App\Models\Tag;
-use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\Return_;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -16,89 +11,14 @@ Route::get('/',function(){
     return view('personal.home');
 })->name('personal.home');
 
-// all the records
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->paginate(3);
 
-    return view('jobs.index',[
-        'jobs'=> $jobs
-    ]);
-})->name('jobs.index');
-
-//create jobs
-Route::get('/jobs/create',function(){
-    return view('jobs.create');
-});
-
-// show
-Route::get('/jobs/{job}',function(Job $job){
-/*   $job = Job::find($job);  
-We do not need to find the job using the id anymore,because we are using route model binding*/
-
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Store
-Route::post('/jobs',function(Request $request){
-    // validation
-    $request->validate([
-        'title'=> 'required|string|min:3',
-        'salary'=> 'required|numeric'
-    ]);
-
-    Job::create([
-        'title'=> request('title'),
-        'salary'=> request('salary'),
-        'employer_id'=> 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// edit job
-Route::get('/jobs/{job}/edit',function(Job $job){
-/*   $job = Job::find($job);  
-We do not need to find the job using the id anymore,because we are using route model binding*/
-
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// update 
-Route::patch('/jobs/{job}',function(Job $job, Request $request){
-    // validation
-    $request->validate([
-        'title'=> 'required|min:3',
-        'salary'=> 'required'
-    ]);
-
-    // authorization(on hold)..
-    // update the job
-    //! method 1
-/*    $job = Job::findorFail($id); 
-We do not need to find the job using the id anymore,because we are using route model binding*/
-
-    // $job->title = $request('title');
-    // $job->salary= $request('salary');
-    // $job->save();
-
-    //! method 2
-    $job->update([
-        'title'=> request('title'),
-        'salary'=> request('salary')
-    ]);
-    // redirect to job page
-    return redirect('/jobs/'.$job->id);
-});
-
-// delete
-Route::delete('/jobs/{job}',function(Job $job){
-    // autorize the req(on hold)
-
-    // $job = Job::findorFail($id);
-    $job->delete();
-
-    return redirect('/jobs');
-});
+Route::get('/jobs',[JobController::class,'index'])->name('jobs.index');;
+Route::get('/jobs/create',[JobController::class,'create']);
+Route::get('/jobs/{job}',[JobController::class,'show']);
+Route::post('/jobs',[JobController::class,'store']);
+Route::get('/jobs/{job}/edit',[JobController::class,'edit']);
+Route::patch('/jobs/{job}',[JobController::class,'update']);
+Route::delete('/jobs/{job}',[JobController::class,'destroy']);
 
 Route::get('/contact',function(){
     return view('personal.contact');
